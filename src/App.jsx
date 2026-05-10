2aaa3bed2641c45c8abeddc2482819622450a439
Mahliyo <gasandigital13@gmail.com>
2026-05-10 12:05:08 +0500

    Now papka

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import "./App.css";
 
 import React from 'react'
 import Cards from "./Components/Cards/Cards";
+import Boxs from "./Components/Boxs/Boxs";
 
 function App() {
   return (
     <>
        <Cards/>
+       </>
     </>
   )
 }
